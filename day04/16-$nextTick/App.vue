d76afaa747b2d10331d789bbf424c3b6060ad82a
Esther <3444104242@qq.com>
2025-11-15 16:08:34 +0800

    Add files via upload

---
+++ day04/16-$nextTick/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app">
+    <div v-if="isShowEdit">
+      <input type="text" v-model="editValue" ref="inp" />
+      <button>确认</button>
+    </div>
+    <div v-else>
+      <span>{{ title }}</span>
+      <button @click="handleEdit">编辑</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '大标题',
+      isShowEdit: false,
+      editValue: '',
+    }
+  },
+  methods: {
+    handleEdit() {
+      // 异步dom更新
+      this.isShowEdit = true
+      // $nextTick 等dom更新完立即执行函数体
+      this.$nextTick(() => {
+        this.$refs.inp.focus()
+      })
+    },
+  },
+}
+</script>
+
+<style></style>
